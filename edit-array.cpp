320521aecd18c018211966fdd1ae80d1c2b8cc11
mlavi16 <Maya.lavi11@gmail.com>
2020-09-09 18:26:55 -0400

    created array

---
+++ edit-array.cpp
@@ -0,0 +1,21 @@
+/*
+Author: Maya Lavi
+Course: CSCI-135
+Instructor: Zamansky
+Assignment: Lab 2C
+
+This program creates an array of 10 integers, 
+and provides the user with an interface to edit any of its elements
+*/
+
+#include <iostream>
+
+int main()
+{
+	int array[10];
+	for (int i = 0; i < 10; i++) {
+		array[i] = 1;
+	}
+
+
+}
