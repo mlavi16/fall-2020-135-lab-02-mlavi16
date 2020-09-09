04e3d0a93ce622f9127e769ea6c905eed3803c22
mlavi16 <Maya.lavi11@gmail.com>
2020-09-09 18:51:25 -0400

    finished edit-array

--- edit-array.cpp
+++ edit-array.cpp
@@ -13,9 +13,31 @@ and provides the user with an interface to edit any of its elements
 int main()
 {
 	int array[10];
+	int index;
+	int value;
+
 	for (int i = 0; i < 10; i++) {
 		array[i] = 1;
 	}
 
+	do {
+		for (int i = 0; i < 10; i++){
+			std::cout << array[i] << " ";
+		}
+		std::cout << std::endl;
 
+
+		std::cout << "Input index: ";
+		std::cin >> index; //assuming input is a number
+
+		std::cout << "Input value: ";
+		std::cin >> value; //assuming input is a number
+
+		array[index] = value;
+
+	} while ((index >= 0) && (index < 10));
+
+	std::cout << "Index out of range. Exit." << std::endl;
+
+	return 0;
 }
