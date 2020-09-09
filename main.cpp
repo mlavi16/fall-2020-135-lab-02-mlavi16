b9f5870d3b871b4aeabb0dbfbdf4e4772173871a
mlavi16 <Maya.lavi11@gmail.com>
2020-09-09 18:21:29 -0400

    changed test cases

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@ Course: CSCI-135
 Instructor: Zamansky
 Assignment: Lab 2B
 
-This program asks the user to input two integers L and U, 
-and prints out all integers in the range L ≤ i < U separated by spaces.
+This program calls print_interval() which prints out all integers in the range L ≤ i < U separated by spaces.
 */
 
 #include <iostream>
@@ -14,11 +13,11 @@ and prints out all integers in the range L ≤ i < U separated by spaces.
 int main()
 {
 	// print_interval tests
-	print_interval(-5, 10); 
-	print_interval(-5, -6);
-	print_interval(0, 0);
-	print_interval(-21, 1);
-	print_interval(1, 8);
+	print_interval(-5, 10); // expected output: -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 
+	print_interval(3, -6); // expected output: 
+	print_interval(0, 0); // expected output: 
+	print_interval(-2001, -1997); // expected output: -2001 -2000 -1999 -1998 
+	print_interval(1, 8); // expected output: 1 2 3 4 5 6 7 
 
 	return 0;
 }
